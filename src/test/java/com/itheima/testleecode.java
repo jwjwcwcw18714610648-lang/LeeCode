860660fdbab22d32bb1d74d15c0fcfef019195d7
JwCw <jwjwcwcw18714610648@gmail.com>
2025-11-19 16:06:17 +0800

    二分搜索查找

--- src/test/java/com/itheima/testleecode.java
+++ src/test/java/com/itheima/testleecode.java
@@ -1713,6 +1713,19 @@ List<List<Integer>> linjietu=new ArrayList<>();
             }
         }return numCourses==CompleteCourse;
     }
+    public int searchInsert(int[] nums, int target) {
+        int n=nums.length;
+        int left=0;
+        int right=n-1;
+        while(left<=right){
+            int mid=(right-left)>>1+left;
+            if(nums[mid]<target){
+                left=mid+1;
+            }else {
+                right=mid-1;
+            }
+        }return left;
+    }
         }
 
 
