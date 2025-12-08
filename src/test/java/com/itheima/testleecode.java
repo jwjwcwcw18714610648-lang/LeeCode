e61a2b0c4f9aac1fab5a56c1a7c604db42453168
JwCw <jwjwcwcw18714610648@gmail.com>
2025-12-08 14:09:46 +0800

    买卖股票

--- src/test/java/com/itheima/testleecode.java
+++ src/test/java/com/itheima/testleecode.java
@@ -2168,7 +2168,20 @@ public class testleecode {
             }
         }
     }
+    public int maxProfit(int[] prices) {
+    int minP=Integer.MAX_VALUE;
+    int maxP=0;
+        for (int i = 0; i < prices.length; i++) {
+                if(prices[i]<minP){
+                    minP=prices[i];
 
+                }
+                if(prices[i]-minP>maxP){
+                    maxP=prices[i]-minP;
+                }
+        }
+        return maxP;
+    }
 }
 
 
